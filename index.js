9c7fecf8ef0fc45a6bcf6cbab04aec6248adc1bb
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-06-26 03:01:49 -0400

    index.html

---
+++ index.js
@@ -0,0 +1,20 @@
+export default async function spinner(){
+    try{
+        const name = document.getElementById("name").value;
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        if(!response.ok){
+            throw new Error("Not around at the moment");
+        }
+        const data = await response.json();
+        const foundation = data.sprites.front_default;
+
+        const stage = document.getElementById("star");
+        stage.src = foundation; 
+        stage.style.display = "block";
+
+        const ocean = document.getElementById("placement"); 
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
