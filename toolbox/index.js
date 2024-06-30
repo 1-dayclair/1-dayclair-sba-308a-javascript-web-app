1a98265987303605c224a078ce9c8fd48df96356
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-06-29 21:40:16 -0400

    toolbox/index.js

--- toolbox/index.js
+++ toolbox/index.js
@@ -16,18 +16,3 @@ async function spinner() {
         console.error(error);
     }
 }
-
-// const success = async function momentum() {
-//     try {
-//         const reply = await fetch ("https://pokeapi.co/api/v2/pokemon/ditto");
-//         console.log("reply")
-//         if(!reply.ok) {
-//             throw new Error("Hoooooly, woooooah there partner, there something that there wrong with this here thingamuhjig. Now, wait a up a New York second...");
-//         }
-//         const load = await reply.json();
-
-//     }
-//     catch(error) {
-//         console.error(error);
-//     }
-// }
