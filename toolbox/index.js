e80c3b374a17143a20ba52135fad096c4f6d4c33
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-06-30 07:02:19 -0400

    toolbox/index.js

--- toolbox/index.js
+++ toolbox/index.js
@@ -1,13 +1,16 @@
 async function spinner() {
     try{
         const name = document.getElementById("name").value;
+        
+        
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
         if(!response.ok){
             throw new Error("Not around at the moment");
         }
         const data = await response.json();
-        const foundation = data.sprites.front_default;
+       
 
+        const foundation = data.sprites.front_default;
         const stage = document.getElementById("star");
         stage.src = foundation; 
         stage.style.display = "block";
